46840a959292bc60ba9c42c547be5ab8d0f4481d
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-09-06 19:01:39 +1000

    XivtoolsWpf integration

--- CustomizePlusWpfUi/App.xaml.cs
+++ CustomizePlusWpfUi/App.xaml.cs
@@ -4,11 +4,18 @@
 namespace CustomizePlusWpfUi
 {
 	using System.Windows;
+	using XivToolsWpf;
 
 	/// <summary>
 	/// Interaction logic for App.xaml.
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			Themes.ApplySystemTheme();
+		}
 	}
 }
--- CustomizePlusWpfUi/MainWindow.xaml.cs
+++ CustomizePlusWpfUi/MainWindow.xaml.cs
@@ -4,11 +4,12 @@
 namespace CustomizePlusWpfUi
 {
 	using System.Windows;
+	using XivToolsWpf.Windows;
 
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml.
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : ChromedWindow
 	{
 		public MainWindow()
 		{
