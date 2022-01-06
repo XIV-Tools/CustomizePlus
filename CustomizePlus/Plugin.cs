ddca3f5cce12047ec66e2393197ce43d0a94f3c1
Luna Nova <git@lunnova.dev>
2022-01-05 20:33:53 -0800

    Hook using Render::Manager::Render

--- CustomizePlus/Plugin.cs
+++ CustomizePlus/Plugin.cs
@@ -3,12 +3,16 @@
 
 namespace CustomizePlus
 {
+	using System;
 	using System.Text;
 	using CustomizePlus.Memory;
+	using Dalamud.Game;
+	using Dalamud.Game.Gui;
 	using Dalamud.Game.ClientState;
 	using Dalamud.Game.ClientState.Objects.SubKinds;
 	using Dalamud.Game.Command;
 	using Dalamud.IoC;
+	using Dalamud.Hooking;
 	using Dalamud.Logging;
 	using Dalamud.Plugin;
 
@@ -18,10 +22,16 @@ namespace CustomizePlus
 
 		private readonly ClientState clientState;
 
+		private delegate IntPtr RenderDelegate(IntPtr renderManager);
+		private readonly Hook<RenderDelegate>? renderManagerHook;
+		private bool updateFailed;
+
 		public Plugin(
 			[RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
 			[RequiredVersion("1.0")] CommandManager commandManager,
-			ClientState clientState)
+			ClientState clientState,
+			SigScanner sigScanner,
+			ChatGui chatGui)
         {
 			PluginInterface = pluginInterface;
 			CommandManager = commandManager;
@@ -36,6 +46,38 @@ namespace CustomizePlus
 			PluginInterface.UiBuilder.OpenConfigUi += Interface.Show;
 
 			this.clientState = clientState;
+
+			try
+			{
+				// "Render::Manager::Render"
+				renderManagerHook = new Hook<RenderDelegate>(sigScanner.ScanText("40 53 55 57 41 56 41 57 48 83 EC 60"), manager =>
+				{
+					// if this gets disposed while running we crash calling Original's getter, so get it at start
+					var original = renderManagerHook.Original;
+					try
+					{
+						if (!updateFailed)
+						{
+							Update();
+						}
+					}
+					catch (Exception e)
+					{
+						chatGui.PrintError("Failed to run CustomizePlus render hook, disabling.");
+						PluginLog.Error($"Error in CustomizePlus render hook {e}");
+
+						updateFailed = true;
+					}
+
+					return original(manager);
+				});
+				// Because scales all get set to 0 below, the character will be very messed up
+				renderManagerHook.Enable();
+			}
+			catch (Exception e)
+			{
+				PluginLog.Error($"Failed to hook Render::Manager::Render {e}");
+			}
 		}
 
 #pragma warning disable CS8618
@@ -53,6 +95,9 @@ namespace CustomizePlus
 			Commands.Dispose();
 			PluginInterface.UiBuilder.Draw -= Interface.Draw;
 			PluginInterface.UiBuilder.OpenConfigUi -= Interface.Show;
+
+			renderManagerHook?.Disable();
+			renderManagerHook?.Dispose();
         }
 
 		public unsafe void Update()
