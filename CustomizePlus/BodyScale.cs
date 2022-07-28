3c93e720bffb3e42704d1bb14e3f788798200127
Yuki <yuuki.walsh@outlook.com>
2022-07-28 12:03:07 +1000

    don't apply root scale if it is the default value 0 or 1.

--- CustomizePlus/BodyScale.cs
+++ CustomizePlus/BodyScale.cs
@@ -17,7 +17,7 @@ namespace CustomizePlus
 
 		public string CharacterName { get; set; } = string.Empty;
 		public Dictionary<string, HkVector4> Bones { get; } = new();
-		public HkVector4 RootScale { get; set; } = HkVector4.One;
+		public HkVector4 RootScale { get; set; } = HkVector4.Zero;
 
 		public unsafe void Apply(Character character)
 		{
@@ -34,11 +34,15 @@ namespace CustomizePlus
 				this.poses[i].Update(obj->Skeleton->PartialSkeletons[i].Pose1);
 			}
 
-			HkVector4 rootScale = obj->Scale;
-			rootScale.X = MathF.Max(this.RootScale.X, 0.01f);
-			rootScale.Y = MathF.Max(this.RootScale.Y, 0.01f);
-			rootScale.Z = MathF.Max(this.RootScale.Z, 0.01f);
-			obj->Scale = rootScale;
+			// Don't apply the root scale if its not set to anything.
+			if (this.RootScale.X != 0 && this.RootScale.Y != 0 && this.RootScale.Z != 0)
+			{
+				HkVector4 rootScale = obj->Scale;
+				rootScale.X = MathF.Max(this.RootScale.X, 0.01f);
+				rootScale.Y = MathF.Max(this.RootScale.Y, 0.01f);
+				rootScale.Z = MathF.Max(this.RootScale.Z, 0.01f);
+				obj->Scale = rootScale;
+			}
 		}
 
 		public void ClearCache()
