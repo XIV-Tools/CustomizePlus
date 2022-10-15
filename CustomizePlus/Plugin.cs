2e8469952e3b19426763eccfe67c8867fc1e3e3e
Fizzy <fizzyvtdev@gmail.com>
2022-10-15 17:28:05 -0400

    Added basic API

--- CustomizePlus/Plugin.cs
+++ CustomizePlus/Plugin.cs
@@ -7,7 +7,9 @@
 namespace CustomizePlus
 {
 	using System;
+	using System.Collections.Concurrent;
 	using System.Collections.Generic;
+	using CustomizePlus.Api;
 	using CustomizePlus.Interface;
 	using Dalamud.Game;
 	using Dalamud.Game.ClientState;
@@ -31,10 +33,12 @@ namespace CustomizePlus
 	{
 		private static readonly Dictionary<string, BodyScale> NameToScale = new();
 		private static Dictionary<GameObject, BodyScale> scaleByObject = new();
+		private static ConcurrentDictionary<string, BodyScale> scaleOverride = new();
 		private static Hook<RenderDelegate>? renderManagerHook;
 		private static BodyScale? defaultScale;
 		private static BodyScale? defaultRetainerScale;
 		private static BodyScale? defaultCutsceneScale;
+		private static CustomizePlusIpc? customizePlusIpc;
 
 		public Plugin()
 		{
@@ -51,6 +55,8 @@ namespace CustomizePlus
 
 				if (PluginInterface.IsDevMenuOpen)
 					ConfigurationInterface.Show();
+
+				customizePlusIpc = new CustomizePlusIpc(ClientState, ObjectTable, PluginInterface);
 
 				ChatGui.Print("Customize+ started");
 			}
@@ -235,6 +241,8 @@ namespace CustomizePlus
 
 		public void Dispose()
 		{
+			customizePlusIpc?.Dispose();
+
 			renderManagerHook?.Disable();
 			renderManagerHook?.Dispose();
 
@@ -293,11 +301,7 @@ namespace CustomizePlus
 			{
 				actorName = new Utf8String(gameObject->Name).ToString();
 
-				if (!string.IsNullOrEmpty(actorName))
-				{
-					NameToScale.TryGetValue(actorName, out scale);
-				}
-				else
+				if (string.IsNullOrEmpty(actorName))
 				{
 					string? actualName = null;
 
@@ -330,8 +334,10 @@ namespace CustomizePlus
 						return null;
 					}
 
-					NameToScale.TryGetValue(actualName, out scale);
+					actorName = actualName;
 				}
+
+				scale = IdentifyBodyScaleByName(actorName);
 			}
 			catch (Exception e)
 			{
@@ -339,6 +345,14 @@ namespace CustomizePlus
 				return null;
 			}
 
+			return scale;
+		}
+
+		private static BodyScale? IdentifyBodyScaleByName(string actorName)
+		{
+			BodyScale? scale = null;
+			if (!scaleOverride.TryGetValue(actorName, out scale))
+				NameToScale.TryGetValue(actorName, out scale);
 			return scale;
 		}
 
@@ -415,5 +429,24 @@ namespace CustomizePlus
 
 		private static string? GetPlayerName()
 			=> ObjectTable[0]?.Name.ToString();
+
+		public static void SetTemporaryCharacterScale(string characterName, BodyScale scale)
+		{
+			if (string.IsNullOrEmpty(characterName))
+				return;
+			scaleOverride[characterName] = scale;
+		}
+
+		public static bool RemoveTemporaryCharacterScale(string characterName)
+		{
+			return scaleOverride.TryRemove(characterName, out _);
+		}
+
+		public static BodyScale? GetBodyScale(string characterName)
+		{
+			if (string.IsNullOrEmpty(characterName))
+				return null;
+			return IdentifyBodyScaleByName(characterName);
+		}
 	}
 }
