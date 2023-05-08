325aa7d016726aa0335622563c5f66b1f770374d
Dendroid <duncanfmiller@gmail.com>
2023-05-08 16:10:52 -0400

    Update configuration version to 3. Add some new constants and predicates, some in peparation for further revision.

--- CustomizePlus/Data/Constants.cs
+++ CustomizePlus/Data/Constants.cs
@@ -1,27 +1,48 @@
 ﻿// © Customize+.
 // Licensed under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace CustomizePlus.Data
 {
 	internal static class Constants
 	{
 		/// <summary>
-		/// Version of the configuration file, when increased a converter should be implemented if necessary
+		/// Version of the configuration file, when increased a converter should be implemented if necessary.
 		/// </summary>
-		public const int ConfigurationVersion = 2;
-		/// <summary>
-		/// Version of the scales imported/exported via clipboard, usually should match ConfigurationVersion
-		/// </summary>
-		public const int ImportExportVersion = ConfigurationVersion;
+		public const int ConfigurationVersion = 3;
 
-		public static Vector3 ZeroVector = new Vector3(0, 0, 0);
-		public static Vector3 OneVector = new Vector3(1, 1, 1);
+		/// <summary>
+		/// Default save location in which to store character profiles. Path will be expanded at runtime
+		/// into regular "My Documents" folder, unless changed by user.
+		/// </summary>
+		public const string DefaultProfileDirectory = @"%UserProfile%\Documents\CustomizePlus\";
+
+		/// <summary>
+		/// The name of the root bone.
+		/// </summary>
+		public const string RootBoneName = "n_root";
+
+		/// <summary>
+		/// How many state changes the editor interface will track before forgetting old ones.
+		/// This is a pretty arbitrary number -- it's not like it's so much data that performance
+		/// would take a hit either way, probably.
+		/// </summary>
+		public const int MaxUndoFrames = 80;
+
+		/// <summary>
+		/// Dalamud object table index for "filler" event objects. Can be ignored(?).
+		/// </summary>
+		public const int ObjectTableFillerIndex = 245;
+
+		/// <summary>
+		/// Predicate function for determining if the given object table index represents a
+		/// cutscene NPC object, with a small buffer in case the prior entries overflow past index 200.
+		/// </summary>
+		public static bool InObjectTableCutsceneNPCRange(int index) => index > 202 && index < 240;
+
+		/// <summary>
+		/// Predicate function for determining if the given object table index represents an
+		/// NPC in a busy area (i.e. there are ~245 other objects already).
+		/// </summary>
+		public static bool InObjectTableBusyNPCRange(int index) => index > 245;
 	}
 }
