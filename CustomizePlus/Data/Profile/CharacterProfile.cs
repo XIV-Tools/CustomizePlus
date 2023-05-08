2cb90bc2867b4caadf44e8a52eb2b49492ea0861
Dendroid <duncanfmiller@gmail.com>
2023-05-08 16:03:54 -0400

    Add to character profiles an internal ID system for better debugging, among other organizational fields. Add a copy constructor and override the equality function.

--- CustomizePlus/Data/Profile/CharacterProfile.cs
+++ CustomizePlus/Data/Profile/CharacterProfile.cs
@@ -11,27 +11,86 @@ using System.Threading.Tasks;
 using Dalamud.Logging;
 using Newtonsoft.Json.Linq;
 using System.Numerics;
+using Newtonsoft.Json;
 
-namespace CustomizePlus.Data
+namespace CustomizePlus.Data.Profile
 {
 	/// <summary>
 	/// Encapsulates the user-controlled aspects of a character profile, ie all of
 	/// the information that gets saved to disk by the plugin.
 	/// </summary>
 	[Serializable]
-	public class CharacterProfile
+	public sealed class CharacterProfile
 	{
-		public string CharacterName { get; set; } = "Default";
-		public string ProfileName { get; set; } = "Profile";
+		[NonSerialized]
+		private static int NextGlobalID = 0;
+		[NonSerialized]
+		private readonly int LocalID;
+
+		public string CharName { get; set; } = "Default";
+		public string ProfName { get; set; } = "Profile";
 		public bool Enabled { get; set; } = false;
 		public DateTime CreationDate { get; set; } = DateTime.Now;
+		public DateTime ModifiedDate { get; set; } = DateTime.Now;
+
+		[JsonIgnore]
 		public int UniqueID => this.CreationDate.GetHashCode();
+
+		[NonSerialized]
+		public string? OriginalFilePath = null;
+		[NonSerialized]
+		public Armature.Armature? Armature = null;
 
 		public Dictionary<string, BoneTransform> Bones { get; init; } = new();
 
+		public CharacterProfile()
+		{
+			this.LocalID = NextGlobalID++;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CharacterProfile"/> class by
+		/// creating a deep copy of the one provided.
+		/// </summary>
+		public CharacterProfile(CharacterProfile original) : this()
+		{
+			this.CharName = original.CharName;
+			this.ProfName = original.ProfName;
+			this.Enabled = original.Enabled;
+			this.CreationDate = original.CreationDate;
+			this.ModifiedDate = DateTime.Now;
+			this.OriginalFilePath = original.OriginalFilePath;
+			this.Armature = null;
+
+			foreach(var kvp in original.Bones)
+			{
+				this.Bones[kvp.Key] = new BoneTransform();
+				this.Bones[kvp.Key].UpdateToMatch(kvp.Value);
+			}
+		}
+
 		public override string ToString()
 		{
-			return $"Profile '{this.ProfileName}' on {this.CharacterName}";
+			return $"Profile ({this.LocalID}) '{this.ProfName}' on {this.CharName}";
+		}
+
+		public override bool Equals(object? obj)
+		{
+			if (obj is CharacterProfile other && other != null)
+			{
+				return this.UniqueID == other.UniqueID
+					&& this.CharName == other.CharName
+					&& this.ProfName == other.ProfName;
+			}
+			else
+			{
+				return base.Equals(obj);
+			}
+		}
+
+		public override int GetHashCode()
+		{
+			return this.UniqueID;
 		}
 	}
 }
